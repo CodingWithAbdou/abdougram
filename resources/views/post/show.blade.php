2614142634e42b332396a194fa7feafda261127c
CodingWithAbdou <coder.abdou@gmail.com>
2023-09-29 15:03:16 +0100

    - add buttons (Follow , Unfollow ..)To Page show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -29,6 +29,16 @@
                         </button>
                     </form>
                 </div>
+                @elseif(auth()->user()->isFollowing($post->owner))
+                <a class="mx-2 nice-btn" href="/{{$post->owner->username}}/unfollow">
+                    {{ __("UnFollow")}}
+                </a>
+                @elseif(auth()->user()->isPending($post->owner))
+                    <span class="text-white bg-gray-400 py-1 px-2 rounded-md">{{ __("Pending")}}</span>
+                @else
+                <a class="mx-2 nice-btn" href="/{{$post->owner->username}}/follow">
+                    {{ __("Follow")}}
+                </a>
                 @endif
             </div>
 
